8ed20694041902ad583adf570760fff77f40085c
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-03-23 11:12:17 +0100

    makes quote more realistic

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const sortable = require('sortablejs')
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   let dots = document.querySelector('#dots')
   dots.style.position = 'relative'
 
-  let addDot = function(){
+  let addDot = function() {
     let content = dots.textContent
     dots.textContent = content + " ."
     dots.style.left = Math.random() * 20 - 10
@@ -13,31 +13,29 @@ document.addEventListener('DOMContentLoaded', function(){
   setInterval(addDot, 17)
 })
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   let el = document.getElementById('items');
   sortable.create(el);
 })
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   let button = document.querySelector('button')
-  button.addEventListener('click', function(){
+  button.addEventListener('click', function() {
     let sequence = fibonacciSequence(39)
     let div = document.createElement('div')
-    div.textContent = sequence.join(' ')
+    div.textContent = `Your quota is ${sequence[5]}.`
     button.parentNode.appendChild(div, button)
   })
 })
 
-function fibonacciSequence(n){
+function fibonacciSequence(n) {
   let sequence = [];
-  for (let i = 0; i <= n; i++){
+  for (let i = 0; i <= n; i++) {
     sequence.push(fibonacci(i));
   }
   return sequence;
 }
 
 function fibonacci(n) {
-   return n < 1 ? 0
-        : n <= 2 ? 1
-        : fibonacci(n - 1) + fibonacci(n - 2);
+  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
